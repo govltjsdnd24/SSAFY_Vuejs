fa8f3a14757dc87d85e1c2f9e95b761af45cc7b3
govtjsdnd24 <38126462+govltjsdnd24@users.noreply.github.com>
2023-11-08 16:05:19 +0900

    1108

---
+++ SSAFY_Vuejs/Vuejs_practice/vue_hw_5_2/component-state-flow-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ParentPage from "./components/ParentPage.vue";
+</script>
+
+<template>
+  <div>
+    <ParentPage/>
+  </div>
+</template>
+
+<style scoped>
+    div{
+        border:1px solid;
+        margin:5px;
+        padding: 5px;
+    }
+</style>
