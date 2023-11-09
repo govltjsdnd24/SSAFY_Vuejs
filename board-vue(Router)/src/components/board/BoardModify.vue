7ea011747328c817fef72475a2eacd4aad4f28e8
chanhong9764 <chanhong9784@naver.com>
2023-11-09 15:15:08 +0900

    11

--- board-vue(Router)/src/components/board/BoardModify.vue
+++ board-vue(Router)/src/components/board/BoardModify.vue
@@ -1,20 +1,20 @@
 <script setup>
-import BoardFormItem from "./item/BoardFormItem.vue";
+import BoardFormItem from './item/BoardFormItem.vue';
 </script>
 
 <template>
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-lg-10">
-        <h2 class="my-3 py-3 shadow-sm bg-light text-center">
-          <mark class="sky">글수정</mark>
-        </h2>
-      </div>
-      <div class="col-lg-10 text-start">
-        <BoardFormItem type="modify" />
-      </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-10">
+                <h2 class="my-3 py-3 shadow-sm bg-light text-center">
+                    <mark class="sky">글수정222</mark>
+                </h2>
+            </div>
+            <div class="col-lg-10 text-start">
+                <BoardFormItem type="modify" />
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped></style>
